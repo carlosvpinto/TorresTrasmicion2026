27ecf31cf83716056fa76f27db9f464e8b199f28
carlos <desoftsis@gmail.com>
2026-09-01 10:14:56 -0400

    Incluye Fotos de Achaguas3

--- scriptachaguas.js
+++ scriptachaguas.js
@@ -45,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         // SEMANA 2
         { src: "imagenes/achaguas/achaguas13-2.jpeg", description: "Trabajos Torres Semana 2 ", week: 2 },
+        
         { src: "imagenes/achaguas/achaguas14-2.jpeg", description: "Trabajos Torres Semana 2 ", week: 2 },
         { src: "imagenes/achaguas/achaguas14-3.jpeg", description: "Trabajos Torres Semana 2 ", week: 2 },
         { src: "imagenes/achaguas/achaguas15-3.jpeg", description: "Trabajos Torres Semana 2 ", week: 2 }
